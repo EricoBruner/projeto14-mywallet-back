dd0fe2dbf0803446d4f6ff70fb8a5dac0ddb4f1f
EricoBruner <ericodasilvabruner@gmail.com>
2023-07-04 20:54:02 -0300

    feito criptografia da senha enviada pelo cliente

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
-import userValidator from "../validators/userValidator";
-import loginValidator from "../validators/loginValidator";
+import userValidator from "../validators/userValidator.js";
+import loginValidator from "../validators/loginValidator.js";
+
+import { db } from "../config/database.js";
 
 const router = Router();
 
 router.post("/cadastro", async (req, res) => {
+  console.log("cadastro");
   const { name, email, password } = req.body;
 
-  const user = { name, email, password };
+  const encryptedPassword = bcrypt.hashSync(password, 10);
+
+  const user = { name, email, password: encryptedPassword };
 
   try {
     const error = userValidator(user);
@@ -36,7 +43,9 @@ router.post("/", async (req, res) => {
 
     const resp = await db.collection("users").findOne({ email });
     if (!resp) return res.sendStatus(404);
-    if (resp.password != password) return res.sendStatus(401);
+
+    if (!bcrypt.compareSync(password, resp.password))
+      return res.sendStatus(401);
 
     return res.sendStatus(200);
   } catch (err) {
@@ -44,4 +53,4 @@ router.post("/", async (req, res) => {
   }
 });
 
-export { router };
+export default router;
