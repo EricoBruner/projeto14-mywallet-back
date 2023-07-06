e12a73c591b903927cef8fa4f6833795bd54f111
EricoBruner <ericodasilvabruner@gmail.com>
2023-07-06 20:44:12 -0300

    ajustado rota de criação de transações

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
-import authRouter from "./routes/auth.routes.js";
-import transactionRouter from "./routes/transaction.routes.js";
+
+import router from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-app.use(authRouter);
-app.use(transactionRouter);
+app.use(router);
 
 app.listen(5000, () => {
   console.log("👾 Servidor rodando na porta 5000! 👾");
--- src/schema/transaction.schema.js
+++ src/schema/transaction.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi";
 
 export const transactionSchema = joi.object({
-  amount: joi.number().required(),
+  amount: joi.number().precision(1).positive().required(),
   description: joi.string().required(),
   type: joi.string().valid("entrada", "saida").required(),
 });
